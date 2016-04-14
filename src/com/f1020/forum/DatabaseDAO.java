d4979c504ff34b2cacd0f3504512ae2def5251d4
梁佳诚 <jcliang1212@163.com>
2016-04-14 15:11:02 +0800

    logintry1

---
+++ src/com/f1020/forum/DatabaseDAO.java
@@ -0,0 +1,57 @@
+package com.f1020.forum;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+
+public class DatabaseDAO {
+   private static Connection connection;
+   private static final String DRIVER="com.mysql.jdbc.Driver";
+   private static final String URL="jdbc:mysql://115.28.34.98:3306/User";
+   private static final String NAME="root";;
+   private static final String PASSWORD="f1020@BUAA";
+   static{
+	   try {
+		 Class.forName(DRIVER);
+	   } catch (ClassNotFoundException e) {
+		// TODO: handle exception
+		   e.printStackTrace();
+	   }
+   }
+   public static Connection getConnection(){
+	  try {
+		connection=DriverManager.getConnection(URL, NAME, PASSWORD);
+		} catch (SQLException e) {
+			e.printStackTrace();
+		// TODO: handle exception
+	  }
+	  return connection;
+   }
+   public static void closeConnection() {
+	   try {
+		  if(connection!=null) connection.close();
+	   } catch(SQLException e) {
+		   e.printStackTrace();
+		// TODO: handle exception
+	   }
+   }
+   public static void closePt(PreparedStatement pt){
+	  try {
+		if(pt!=null) pt.close();
+	  } catch (SQLException e) {
+		  e.printStackTrace();
+		// TODO: handle exception
+	  }
+   }
+   public static void closeRs(ResultSet rs) {
+	   try {
+		  if(rs!=null) rs.close();
+	   } catch (SQLException e) {
+		  // TODO: handle exception
+		  e.printStackTrace();
+	   }
+   }
+}
